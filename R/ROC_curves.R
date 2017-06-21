2e010c2d71d79b8a1dba666887bdb474719c82dd
DrRadan <rosoniki@gmail.com>
2017-06-21 15:46:20 -0400

    export histogram option and range of p-values determined by dataset

--- R/ROC_curves.R
+++ R/ROC_curves.R
@@ -8,6 +8,7 @@
 #' @param data_frame the dataframe containing clinical data
 #' @param response_name the column name of the data_frame that has the resonse variable
 #' @param fitted_model the stanreg fitted model
+#' @param historgam do you want a histogram of all the estimated posterior p-values? Possible values TRUE/FALSE, defaults to FALSE
 #'
 #' @return numeric AUC
 #' @import survminer
@@ -19,33 +20,34 @@
 #' @import rstanarm
 #' @import zoo
 #' @export
-auc <- function(data_frame, response_name, fitted_model){
+auc <- function(data_frame, response_name, fitted_model, histogram=FALSE){
   var = data_frame[response_name][,1]  #observed outcomes
   ind_vars = data_frame[ , ! colnames(data_frame) %in% c(response_name) ] #only independent variables
-  p_data <- posterior_predict(fitted_model, newdata=ind_vars)
+  p_data <- posterior_predict(fitted_model, newdata=data_frame)
   p_est <- round(apply(p_data, 2, sum)/dim(p_data)[1], 3) #calculate p-values
-  
+  if (histogram=TRUE) {hist(p_est, xlab= "Estimated posterior p-values")}
+
   tDF <- tbl_df(data.frame(var=var, p_est=p_est))
-  br <- seq(from=0, to=1, length.out=1000)
+  br <- seq(from=min(p_est), to=max(p_est), length.out=1000)
   TPR <- c()
   FPR <- c()
-  
+
   for (i in br){
     tDF1 <- filter(tDF, p_est<=i)
     tDF0 <- filter(tDF, p_est>i)
-    
+
     size1 <- dim(tDF1)[1]
     size0 <- dim(tDF0)[1]
-    
+
     true_pos =  sum(tDF1$var)
     false_pos = size1 - true_pos
     true_neg = size0 - sum(tDF0$var)
     false_neg = sum(tDF0$var)
-    
+
     TPR <- c(TPR, true_pos/(true_pos+false_neg))
     FPR <- c(FPR, 1-(true_neg/(true_neg+false_pos)))
   }
-  
+
   AUC <- sum(diff(TPR[id])*rollmean(FPR[id],2))
   return(AUC)
 }
